ad2e39e14fba2622c3a902a7d08520ccc4a30618
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-05-10 19:30:11 +0300

    свсввс

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,16 +1,10 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-const ul =document.querySelector(`.gallery`);
-
-var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
-console.log(lightbox);
-const crEl = galleryItems.map(galleryItem => 
-    `<a class="gallery__item" href="${galleryItem.original}">
-    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
-  </a>`
-    
-    ).join(" ");
-    ul.insertAdjacentHTML('afterbegin', crEl);
+import "simplelightbox/dist/simple-lightbox.min.css";import { galleryItems } from './gallery-items.js';// Change code below this line
+// console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+function createGalleryMarkup(galleryItems) {    return galleryItems.map(item => {        return ` <a class="gallery__item" href="${item.original}">  <img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a>  `    }).join("");
+}
+const galleryMarkup = createGalleryMarkup(galleryItems);galleryList.innerHTML = galleryMarkup;
+galleryList.addEventListener("click", onGalleryItemClick);
+function onGalleryItemClick(event) {    event.preventDefault();    const isGalleryImage = event.target.classList.contains("gallery__image");    if (!isGalleryImage) {        return;    }}
+let gallery = new SimpleLightbox('.gallery a', {    captionsData: "alt",    captionDelay: 250,});
