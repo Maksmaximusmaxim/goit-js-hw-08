af40e255736a2960d7aeb441ff3593c2ac6ee672
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-05-10 17:52:45 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,16 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const ul =document.querySelector(`.gallery`);
 
-console.log(galleryItems);
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+console.log(lightbox);
+const crEl = galleryItems.map(galleryItem => 
+    `<a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+  </a>`
+    
+    ).join(" ");
+    ul.insertAdjacentHTML('afterbegin', crEl);
