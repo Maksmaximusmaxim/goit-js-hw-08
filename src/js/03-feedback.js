e2d2d37da6a17f2cc1717c73250cd29cd480a8ba
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-05-12 09:41:26 +0300

    KN/LKN

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,30 @@
-var throttle = require('lodash.throttle');
+    var throttle = require('lodash.throttle');
 
-const form = document.querySelector(`.feedback-form`);
-
-
+   const form= document.querySelector(".feedback-form")
 
 
 
+const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+form.addEventListener("input", throttle(
+    (e) =>{
+        formData[e.target.name] = e.target.value;
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    }
+    , 500));
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    console.log(formData);
+    e.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+});
+populateTextArea()
 
 
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
+function populateTextArea() {
+    const savedMasseg = localStorage.getItem("feedback-form-state");
+    const parsedsavedMasseg = JSON.parse(savedMasseg)
+    if (savedMasseg) {
+        form.elements.message.value = parsedsavedMasseg.message ;
+        form.elements.email.value = parsedsavedMasseg.email ;
+    }
+}
