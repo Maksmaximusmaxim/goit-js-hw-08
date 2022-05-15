3e4daf65a04b73625a4a216edbb1441b8fc46f32
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-05-15 20:17:24 +0300

    оиои

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,5 +4,7 @@ var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
+
 player.on('timeupdate',throttle((e)=>{ localStorage.setItem(`videoplayer-current-time`, e.seconds)},1000 ));
-player.setCurrentTime(localStorage.getItem(`videoplayer-current-time`));
+const filterCurrentTime = localStorage.getItem(`videoplayer-current-time`) ? localStorage.getItem(`videoplayer-current-time`) : 0;
+player.setCurrentTime(filterCurrentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,8 @@
     var throttle = require('lodash.throttle');
 
-   const form= document.querySelector(".feedback-form")
-
+   const form= document.querySelector(".feedback-form");
+   const textarea = document.querySelector(`textarea`);
+   const input= document.querySelector('.feedback-form input')
 
 
 const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
@@ -13,10 +14,17 @@ form.addEventListener("input", throttle(
     , 500));
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
+    if (!formData[input.name] || !formData[textarea.name]) {
+        alert("вернись и заполни");
+    }
+    else {
+
     console.log(formData);
     e.currentTarget.reset();
-    localStorage.removeItem("feedback-form-state");
-});
+    localStorage.removeItem('feedback-form-state');
+    formData[input.name] = '';
+    formData[textarea.name] = '';
+}});
 populateTextArea()
 
 
@@ -24,7 +32,7 @@ function populateTextArea() {
     const savedMasseg = localStorage.getItem("feedback-form-state");
     const parsedsavedMasseg = JSON.parse(savedMasseg)
     if (savedMasseg) {
-        form.elements.message.value = parsedsavedMasseg.message ;
-        form.elements.email.value = parsedsavedMasseg.email ;
+        form.elements.message.value = parsedsavedMasseg.message || "" ;
+        form.elements.email.value = parsedsavedMasseg.email || "" ;
     }
 }
